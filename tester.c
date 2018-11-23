ee1866f490a40c5887e493c615ab7c9942849759
paulorcmendes <mendesdpn@gmail.com>
2018-11-22 22:18:11 -0300

    ds

--- tester.c
+++ tester.c
@@ -8,17 +8,10 @@ int main(){
     FILE *outFile;
 
     for(i = 1; i <= 4; i++){
-<<<<<<< HEAD
-        outFile = fopen("somaComum.txt", "a");
-        fprintf(outFile, "\n############ %d PROCESSOS ############\n", i);
-        fclose(outFile);
-        sprintf(comando, "mpiexec -n %d somaComum.exe", i);
-=======
         outFile = fopen("somaLog.txt", "a");
         fprintf(outFile, "\n############ %d PROCESSOS ############\n", i);
         fclose(outFile);
         sprintf(comando, "mpiexec -n %d somaLog.exe", i);
->>>>>>> fccef233ee67045c1389a6e633dadc7133c3390c
         for(j = 0; j<5; j++){
             system(comando);
         }
